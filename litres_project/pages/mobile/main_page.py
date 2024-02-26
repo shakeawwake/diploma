df80a80b01dacfd28a5e77122e4c5b9497edf205
kglazova <kglazova@huntflow.ru>
2024-02-26 16:33:40 +0300

    tm deleted

--- litres_project/pages/mobile/main_page.py
+++ litres_project/pages/mobile/main_page.py
@@ -6,13 +6,13 @@ class AndroidMainPage:
 
     def selecting_application_language(self):
 
-        if browser.element((AppiumBy.ID, "ru.litres.android:id/choosebutton")).with_(timeout=20).matching(be.clickable):
+        if browser.element((AppiumBy.ID, "ru.litres.android:id/choosebutton")).matching(be.clickable):
             browser.element((AppiumBy.ID, "ru.litres.android:id/choosebutton")).click()
         return self
 
     def hiding_adult_content(self):
 
-        if browser.element((AppiumBy.ID, "ru.litres.android:id/btnDisableAdultContent")).with_(timeout=25).click():
+        if browser.element((AppiumBy.ID, "ru.litres.android:id/btnDisableAdultContent")).click():
             browser.element((AppiumBy.ID, "ru.litres.android:id/btnConfirmDisableAdultContent")).click()
         return self
 
